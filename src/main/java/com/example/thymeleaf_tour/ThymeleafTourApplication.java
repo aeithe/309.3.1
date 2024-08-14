3b030d6056eab87ce1a4bb54cab6d5929c889a03
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-08-14 12:01:55 -0400

    first and last commit

---
+++ src/main/java/com/example/thymeleaf_tour/ThymeleafTourApplication.java
@@ -0,0 +1,14 @@
+package com.example.thymeleaf_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages = "com.example")
+public class ThymeleafTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTourApplication.class, args);
+	}
+
+}
